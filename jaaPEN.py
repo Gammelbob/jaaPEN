da429b760b41873344e13da869a0e7d867562bd9
Gammelbob <spam@kh3a.de>
2011-01-12 18:31:21 +0100

    added POST detection

--- jaaPEN.py
+++ jaaPEN.py
@@ -26,6 +26,7 @@
 # https://github.com/Gammelbob/jaaPEN
 #
 # Changelog:
+# 0.0.7 - added POST detection
 # 0.0.6 - better performance by reducing sql-queries and rewritten linkhandling
 # 0.0.5 - added project table to store data for more than one site
 # 0.0.4 - switched over to sqlite. some local vars are still in use.
@@ -37,9 +38,9 @@
 #2do: db-qry replace % with ?
 #more at grep -Hirn "2do:" .
 #issues:
-# - try: tmp = storage[baselink][key][value] except KeyError: is bad in many ways.
+# - try: tmp = storage[baselink][key][value] except KeyError: is bad and just ugly
 # if not storage[baselink][key][value]: would be nicer but i did not get it to work
-# - 'myID' in local dict storage[..] could be a key or val at the target site
+# - 'myID' in local dict storage[..] could be a key or val at the remote host
 ######################################################################################
 
 import sys
@@ -53,10 +54,12 @@ try:
 except ImportError:
     import sqlite3 as sqlite
 
-version = '0.0.6'
+version = '0.0.7'
 showResults = False
 saveResults = False
+
 filterExternal = True
+scanForms = True
 projectID = 1
 dbfile = ':memory:'
 #dbfile = '/tmp/jaaPEN'
@@ -75,7 +78,8 @@ except:
     print "# Example 1 will display the results as a tree."
     print "# Example 2 will display the results as a tree."
     print "# Example 3 will display nothing but errors."
-    print "# Example 4 will display nothing but errors. The results will be saved in the given file as a tree."
+    #print "# Example 4 will display nothing but errors. The results will be saved in the given file as a tree."
+    print "# Example 4 will display nothing but errors."
     print "# Runtime stats will be displayed on all examples."
     exit()
 
@@ -83,6 +87,7 @@ try:
     if sys.argv[2] == 'show':
         showResults = True
     elif sys.argv[2] == 'save':
+        print '#\t* results will not be saved. nothing but errors and simple statistics will be displayed.\n#\t* use jaaPEN host > file instead\n#'
         showResults = False
         #2do: if iswritable(sys.argv[3]): set saveResults = True, remove showResults here as it is set to False on init
 except:
@@ -103,6 +108,13 @@ external = set([])
 storage = dict()
 
 linkregex = re.compile('<a\s*href=[\'|"](.*?)[\'"].*?>')
+if scanForms:
+    formRegex = re.compile('<form.*?</form>',re.DOTALL)
+    formActionRegex = re.compile('action=[\'|"](.*?)[\'|"]')
+    formMethodRegex = re.compile('method=[\'|"](.*?)[\'|"]')
+    inputRegex = re.compile('<input.*?>')
+    inputNameRegex = re.compile('name=[\'|"](.*?)[\'"]')
+    inputValueRegex = re.compile('value=[\'|"](.*?)[\'"]')
 
 try:
     print '# loading previous results from db'
@@ -111,12 +123,13 @@ try:
         storage[baselinkRow[1]] = dict()
         storage[baselinkRow[1]]['myID'] = baselinkRow[0]
         for typeRow in cursor.execute("SELECT type FROM `keys` WHERE baseID = %i GROUP BY `keys`.type" % baselinkRow[0]).fetchall():
+            storage[baselinkRow[1]][typeRow[0]] = dict()
             for keyRow in cursor.execute("SELECT keyID, key  FROM `keys` WHERE baseID = %i AND type='%s'" % (baselinkRow[0], typeRow[0])).fetchall():
-                storage[baselinkRow[1]][keyRow[1]] = dict()
-                storage[baselinkRow[1]][keyRow[1]]['myID'] = keyRow[0]
+                storage[baselinkRow[1]][typeRow[0]][keyRow[1]] = dict()
+                storage[baselinkRow[1]][typeRow[0]][keyRow[1]]['myID'] = keyRow[0]
                 for valueRow in cursor.execute("SELECT valueID, value FROM `valuaes` WHERE keyID = %i" % keyRow[0]).fetchall():
-                    storage[baselinkRow[1]][keyRow[1]][valueRow[1]] = dict()
-                    storage[baselinkRow[1]][keyRow[1]][valueRow[1]]['myID'] = valueRow[0]
+                    storage[baselinkRow[1]][typeRow[0]][keyRow[1]][valueRow[1]] = dict()
+                    storage[baselinkRow[1]][typeRow[0]][keyRow[1]][valueRow[1]]['myID'] = valueRow[0]
 except:
     print '# loading previous results from DB failed. guessing new table or in-memory DB'
 
@@ -134,8 +147,10 @@ while True:
         pagesPerSec = len(crawled) / jobtime
         jobtime = int(jobtime)
         cntBaseLinks = cursor.execute('SELECT count(baseID) FROM baselinks WHERE projectID = %i' % projectID).fetchone()[0]
-        cntGetKeys = cursor.execute('SELECT count(keyID) FROM keys WHERE baseID IN (SELECT baseID FROM baselinks WHERE projectID = %i)' % projectID).fetchone()[0]
-        cntGetValues = cursor.execute('SELECT count(valueID) FROM valuaes WHERE keyID IN (SELECT keyID FROM keys WHERE baseID IN (SELECT baseID FROM baselinks WHERE projectID = %i))' % projectID).fetchone()[0]
+        cntGetKeys = cursor.execute('SELECT count(keyID) FROM keys WHERE type = \'%s\' AND baseID IN (SELECT baseID FROM baselinks WHERE projectID = %i)' % ('GET',projectID)).fetchone()[0]
+        cntGetValues = cursor.execute('SELECT count(valueID) FROM valuaes WHERE keyID IN (SELECT keyID FROM keys WHERE type = \'%s\' AND baseID IN (SELECT baseID FROM baselinks WHERE projectID = %i))' % ('GET',projectID)).fetchone()[0]
+        cntPostKeys = cursor.execute('SELECT count(keyID) FROM keys WHERE type = \'%s\' AND baseID IN (SELECT baseID FROM baselinks WHERE projectID = %i)' % ('POST',projectID)).fetchone()[0]
+        cntPostValues = cursor.execute('SELECT count(valueID) FROM valuaes WHERE keyID IN (SELECT keyID FROM keys WHERE type = \'%s\' AND baseID IN (SELECT baseID FROM baselinks WHERE projectID = %i))' % ('POST',projectID)).fetchone()[0]
         if showResults:
             for baselinkRow in cursor.execute("SELECT baseID, baselink FROM `baselinks` WHERE projectID = %i" % projectID).fetchall():
                 print '%s' % baselinkRow[1]
@@ -153,6 +168,8 @@ while True:
         print '#  found baselinks\t%i' % cntBaseLinks
         print '#  found get-keys\t%i' % cntGetKeys
         print '#  found get-values\t%i' % cntGetValues
+        print '#  found post-keys\t%i' % cntPostKeys
+        print '#  found post-values\t%i' % cntPostValues
         print '######'
         # commit our changes which is only needed at a physical file, not at an in-memory database.
         connection.commit()
@@ -223,24 +240,98 @@ while True:
             if '=' in parameter:
                 key = parameter.split('=')[0]
                 value = parameter.split('=')[1]
+                try:
+                    tmp = storage[baselink]['GET']
+                except KeyError:
+                    storage[baselink]['GET'] = dict()
                 # lets check if we already know this key in relation to our baselink
                 try:
-                    tmp = storage[baselink][key]['myID']
+                    tmp = storage[baselink]['GET'][key]['myID']
                 except KeyError:
                     cursor.execute("INSERT INTO `keys` VALUES(NULL,%i,'%s','%s')" % (storage[baselink]['myID'],'GET',key))
-                    storage[baselink][key] = dict()
-                    storage[baselink][key]['myID'] = cursor.lastrowid
+                    storage[baselink]['GET'][key] = dict()
+                    storage[baselink]['GET'][key]['myID'] = cursor.lastrowid
                     #print "\t\tnew key"
                 # lets check if we already know this value in relation to our key
                 try:
-                    tmp = storage[baselink][key][value]['myID']
+                    tmp = storage[baselink]['GET'][key][value]['myID']
                 except KeyError:
-                    cursor.execute("INSERT INTO `valuaes` VALUES (NULL,%i,'%s')" % (storage[baselink][key]['myID'],value))
-                    storage[baselink][key][value] = dict()
-                    storage[baselink][key][value]['myID'] = cursor.lastrowid
+                    cursor.execute("INSERT INTO `valuaes` VALUES (NULL,%i,'%s')" % (storage[baselink]['GET'][key]['myID'],value))
+                    storage[baselink]['GET'][key][value] = dict()
+                    storage[baselink]['GET'][key][value]['myID'] = cursor.lastrowid
                     #print "\t\t\tnew value"
             else:
                 print '# found a weird parameter: %s' % parameter
         #connection.commit()
         tocrawl.add(link)
+
+    if not scanForms:
+        continue
+    # get forms
+    forms = formRegex.findall(msg)
+    for form in (forms.pop(0) for _ in xrange(len(forms))):
+        formAction = formActionRegex.findall(form)[0]
+        formMethod = formMethodRegex.findall(form)[0].upper()
+        # lets do some path fixing and blacklisting
+        if not formAction.startswith('http'):
+            if formAction.startswith('/'):
+                formAction = 'http://' + url[1] + formAction
+            elif formAction.startswith('#'):
+                formAction = 'http://' + url[1] + url[2] + formAction
+            elif ':' in formAction:
+                print '# found a weird formAction: ' + formAction
+                print '# nevertheless, we turn on'
+                #blacklist.add(formAction)
+                #continue
+            else:
+                formAction = 'http://' + url[1] + '/' + formAction
+        if not formAction.startswith(sys.argv[1]):
+            if filterExternal:
+                print '# found an external form target: ' + formAction
+                print '# breaking up here!'
+                blacklist.add(formAction)
+                external.add(formAction)
+                continue
+        if formAction in blacklist:
+            continue
+        if formAction not in crawled and formAction not in tocrawl:
+            # lets have a look what do we get if we request the forms target without any parameters
+            tocrawl.add(formAction)
+        # lets check if we already know the forms target/baselink
+        try:
+            tmp = storage[formAction]['myID']
+        except KeyError:
+            cursor.execute("INSERT INTO `baselinks` VALUES(NULL,%i,'%s')" % (projectID,formAction))
+            storage[formAction] = dict()
+            storage[formAction]['myID'] = cursor.lastrowid
+        # do we have an [POST/GET/WHATEVER] array for current form target
+        try:
+            tmp = storage[formAction][formMethod]
+        except KeyError:
+            storage[formAction][formMethod] = dict()
+        # finally get parameters
+        inputs = inputRegex.findall(form)
+        for inputelement in (inputs.pop(0) for _ in xrange(len(inputs))):
+            try:
+                inputName = inputNameRegex.findall(inputelement)[0]
+            except IndexError:
+                inputName = 'not/set' 
+            try:
+                inputValue = inputValueRegex.findall(inputelement)[0]
+            except IndexError:
+                inputValue = 'not/set'
+            # lets check if we know the key in relation to our formAction/baselink
+            try:
+                tmp = storage[formAction][formMethod][inputName]['myID']
+            except KeyError:
+                cursor.execute("INSERT INTO `keys` VALUES(NULL,%i,'%s','%s')" % (storage[formAction]['myID'],formMethod,inputName))
+                storage[formAction][formMethod][inputName] = dict()
+                storage[formAction][formMethod][inputName]['myID'] = cursor.lastrowid
+            # lets check if we know the value in relation to the key
+            try:
+                tmp = storage[formAction][formMethod][inputName][inputValue]['myID']
+            except KeyError:
+                cursor.execute("INSERT INTO `valuaes` VALUES (NULL,%i,'%s')" % (storage[formAction][formMethod][inputName]['myID'],inputValue))
+                storage[formAction][formMethod][inputName][inputValue] = dict()
+                storage[formAction][formMethod][inputName][inputValue]['myID'] = cursor.lastrowid
 
